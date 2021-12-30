87ceb1e77649eb033917a94ded3a6be49f3a10b6
antdev1987 <antdev1987@gmail.com>
2021-12-29 21:46:47 -0600

    51. creando y llamando nuestro primer componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import Header from './components/Header'
 
 function App() {
   
   return (
-    <div className="App">
-      <h1>hola mundo</h1>
-    </div>
+    <>
+     <Header />
+    </>
   )
 }
 
-export default App
+export default App 
