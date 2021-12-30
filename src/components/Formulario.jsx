1185be70c510bb94a617f3fd2a7b52aa02ca0e44
antdev1987 <antdev1987@gmail.com>
2021-12-30 03:26:54 -0600

    65/66 leyendo info de un input y eventos y sintaxis en react

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,15 @@
 import React, {useState} from 'react'
 
 const Formulario = () => {
+   const [nombre, setNombre] = useState('')
+
+   const handleSubmit =(e)=>{
+      e.preventDefault()
+      console.log('enviando submit')
+
+   }
+
+
    return (
       <div className='md:w-1/2 lg:w-2/5'>
          <h2 className='font-black text-3xl text-center'>Seguimiento Pacientes</h2>
@@ -10,7 +19,7 @@ const Formulario = () => {
             <span className='text-indigo-600 font-bold'>Administralos</span>
          </p>
 
-         <form className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'> 
+         <form onSubmit={handleSubmit} className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'> 
             <div className='mb-5'>
                <label htmlFor='mascota' className='block text-gray-700 uppercase font-bold'>Nombre Mascota</label>
                <input
@@ -18,6 +27,8 @@ const Formulario = () => {
                   type='text'
                   placeholder='Nombre de la Mascota'
                   className='border-2 w-full p-2 mt-2 rounded-md'
+                  value={nombre}
+                  onChange={(e)=>{setNombre(e.target.value)}}
                />
             </div>
 
