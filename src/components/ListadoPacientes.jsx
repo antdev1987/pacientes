01ec3130bb80691a1640f44f380ff81a15e02a65
antdev1987 <antdev1987@gmail.com>
2022-01-01 00:30:56 -0600

    77. Mostrar textos de forma condicional en React

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,27 +1,51 @@
 import React from 'react'
 import Paciente from './Paciente'
 
-const ListadoPacientes = ({pacientes}) => {
+const ListadoPacientes = ({ pacientes }) => {
 
-   console.log(pacientes)
+   console.log(pacientes.length)
+
+   console.log(!(pacientes.length))
 
    return (
-      <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
 
-         <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
-         <p className='text-xl mt-5 mb-10 text-center'>
-            Administras tus {''}
-            <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
-         </p>
+      <>     
+         {/* se renderizara si no hay nada en el array */}
+         {!(pacientes.length) && (
+            <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
 
-         {pacientes.map(paciente =>
-            (
-               <Paciente key={paciente.id} paciente={paciente} />
-            )
+               <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
+               <p className='text-xl mt-5 mb-10 text-center'>
+                  No hay Pacientes {''}
+                  <span className='text-indigo-600 font-bold'>Crea una Nuevo</span>
+               </p>
+
+
+
+            </div>
          )}
-        
-         
-      </div>
+
+         {/* se renderizara si hay datos en el array */}
+         {!!pacientes.length && (
+            <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
+
+               <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
+               <p className='text-xl mt-5 mb-10 text-center'>
+                  Administras tus {''}
+                  <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+               </p>
+
+               {pacientes.map(paciente =>
+               (
+                  <Paciente key={paciente.id} paciente={paciente} />
+               )
+               )}
+
+
+            </div>
+         )}
+
+      </>
    )
 }
 
